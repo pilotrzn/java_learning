a78950a34942ab3b01d94732d8ba078eea691690
Alex Avo <grant5518@yandex.ru>
2023-02-07 22:13:58 +0300

    update dz

--- oop-lesson-7/src/com/alex/oop/dz/BuildHouseRunner.java
+++ oop-lesson-7/src/com/alex/oop/dz/BuildHouseRunner.java
@@ -15,19 +15,19 @@ package com.alex.oop.dz;
 
 public class BuildHouseRunner {
     public static void main(String[] args) {
-        Stage stage = new Stage(4, 4);
-        stage.print();
+        //Stage stage = new Stage(4, 4);
+        //stage.print();
         //stage.printAllInformation();
 
-        Flat flat = new Flat(23, 3);
-        flat.print();
+        //Flat flat = new Flat(23, 3);
+        //flat.print();
         //flat.printAllinformation();
 
-        Room room = new Room(false,2);
-        room.print();
+        //Room room = new Room(false,2);
+        //room.print();
 
-        House house = new House(12, 10, 5);
-        house.print();
+        House house = new House(12, 22, 6);
+       // house.print();
         house.printAllInformation();
 
     }
--- oop-lesson-7/src/com/alex/oop/dz/Stage.java
+++ oop-lesson-7/src/com/alex/oop/dz/Stage.java
@@ -3,6 +3,7 @@ package com.alex.oop.dz;
 public class Stage {
     private int stageNumber;
     private Flat[] flats;
+    public static int flatNumber = 1;
 
     public Stage(int stageNumber, int flatsOnStage) {
         this.stageNumber = stageNumber;
@@ -19,7 +20,8 @@ public class Stage {
 
     private Flat[] createFlats(int flatOnStage) {
         Flat[] flatsArr = new Flat[flatOnStage];
-        int flatNumber = (stageNumber == 1) ? 1 : stageNumber * flatOnStage - flatOnStage + 1;
+        // int flatNumber = (stageNumber == 1) ? 1 : stageNumber * flatOnStage -
+        // flatOnStage + 1;
         for (int i = 0; i < flatOnStage; i++) {
             int roomsCount = (i == 0 || i == 2) ? 3 : 2;
             flatsArr[i] = new Flat(flatNumber, roomsCount);
